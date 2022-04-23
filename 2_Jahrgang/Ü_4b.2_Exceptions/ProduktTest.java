6f828f482525254061a8aeeb088a81415cf58d9f
Kacper Bohaczyk <KacperBohaczyk@gmail.com>
2022-04-23 20:21:33 +0200

    Update ProduktTest.java

--- 2_Jahrgang/Ü_4b.2_Exceptions/ProduktTest.java
+++ 2_Jahrgang/Ü_4b.2_Exceptions/ProduktTest.java
@@ -2,7 +2,7 @@ package src;
 
 /**
  * Diese Klasser ist die Main Klasse der Produkte
- * Es wurden noch zusätzlich in der neusten Version Testvorgänge für die Exceptions hinzugefügt
+ * Es wurden noch zusÃ¤tzlich in der neusten Version TestvorgÃ¤nge fÃ¼r die Exceptions hinzugefÃ¼gt
  * @author 	Kacper Bohaczyk
  * @date 	22-04-2022
  */
@@ -11,19 +11,24 @@ public class ProduktTest {
 	/**
 	 * Ist die main Klasse
 	 * @param args 	Argumente
-	 * @throws NumericRangeException	Diese Exception catcht ob die rübergegebenen Parameter im gültigem Bereich sind
+	 * @throws NumericRangeException	Diese Exception catcht ob die rÃ¼bergegebenen Parameter im gÃ¼ltigem Bereich sind
 	 */
 	public static void main(String[]args) throws NumericRangeException {
-		Produkt p1 = new Produkt(987654321, "Mappe rot", "eine dünne rote Mappe", 2.49);
-		Produkt p2 = new Produkt(987654321, "Mappe rot", "eine dünne rote Mappe", 2.49);
-		Produkt p3 = new Produkt(15082005, "Buntstift", "Ein grüner Stift", 2.50);
-		// Produkt p4 = new Produkt(2005, "Buch", "Ein fantasy Buch", 4.50);		// Hier kann ein Test ausgeführt werden ob die Exceptions funktionieren in der Klasse Produkt
+		Produkt p1 = new Produkt(987654321, "Mappe rot", "eine dÃ¼nne rote Mappe", 2.49);
+		Produkt p2 = new Produkt(987654321, "Mappe rot", "eine dÃ¼nne rote Mappe", 2.49);
+		Produkt p3 = new Produkt(15082005, "Buntstift", "Ein grÃ¼ner Stift", 2.50);
+		// Produkt p4 = new Produkt(2005, "Buch", "Ein fantasy Buch", 4.50);		
+		/*
+		 * Hier kann ein Test ausgefÃ¼hrt werden ob die Exceptions funktionieren in der Klasse Produkt
+		 */
 		
-		
-		ErweitertesProdukt pe1 = new ErweitertesProdukt(15082005, "Radiergummi", "Ein Radiergummi zum löschen", 3.0 , 20);
-		ErweitertesProdukt pe2 = new ErweitertesProdukt(15082005, "Radiergummi", "Ein Radiergummi zum löschen", 3.0 , 20);
+		ErweitertesProdukt pe1 = new ErweitertesProdukt(15082005, "Radiergummi", "Ein Radiergummi zum lÃ¶schen", 3.0 , 20);
+		ErweitertesProdukt pe2 = new ErweitertesProdukt(15082005, "Radiergummi", "Ein Radiergummi zum lÃ¶schen", 3.0 , 20);
 		ErweitertesProdukt pe3 = new ErweitertesProdukt(23221315, "Bleistift", "Ein Bleistift zum schreiben", 2.0 , 10);
-		// ErweitertesProdukt pe4 = new ErweitertesProdukt(23223, "Kuli", "Ein Bleistift zum schreiben", 2.0 , -10); 	// Hier kann ein Test ausgeführt werden ob die Exceptions funktionieren in der Klasse ErweitertesProdukt
+		// ErweitertesProdukt pe4 = new ErweitertesProdukt(23223, "Kuli", "Ein Bleistift zum schreiben", 2.0 , -10); 	
+		/*
+		 * Hier kann ein Test ausgefÃ¼hrt werden ob die Exceptions funktionieren in der Klasse ErweitertesProdukt
+		 */ 
 		pe1.setBewertungen(4);	
 
 		Bewertung bewertung = new Bewertung(3.0,"Ziemlich gut");
@@ -34,33 +39,33 @@ public class ProduktTest {
 		 * Hier wird alles Vorbereitet um die Methoden zu testen
 		 */
 		
-		System.out.println(p3.gesamtPreis(3));					// Testet aus ob die Methode gesamtPreis richtig funktioniert
-		System.out.println(p3.toString());						// Testet aus ob alle Daten rübergegeben werden
-		System.out.println("Vergleich: " + p1.equals(p2));		// vergleicht ob die Methode equals aus der Klasse "Produkt" funktioniert (solte true zurückgeben)
-		System.out.println("Vergleich: " + p1.equals(p3));		// vergleicht ob die Methode equals aus der Klasse "Produkt" funktioniert (solte false zurückgeben)
-		System.out.print(p1.hashCode());						// führt die Methode hashCode aus der Klasse "Produkt" aus
+		System.out.println(p3.gesamtPreis(3));				// Testet aus ob die Methode gesamtPreis richtig funktioniert
+		System.out.println(p3.toString());				// Testet aus ob alle Daten rÃ¼bergegeben werden
+		System.out.println("Vergleich: " + p1.equals(p2));		// vergleicht ob die Methode equals aus der Klasse "Produkt" funktioniert (solte true zurÃ¼ckgeben)
+		System.out.println("Vergleich: " + p1.equals(p3));		// vergleicht ob die Methode equals aus der Klasse "Produkt" funktioniert (solte false zurÃ¼ckgeben)
+		System.out.print(p1.hashCode());				// fÃ¼hrt die Methode hashCode aus der Klasse "Produkt" aus
 		/**
-		 * Hier werden alle Methoden für die Klasse: "Produkt" getestet
+		 * Hier werden alle Methoden fÃ¼r die Klasse: "Produkt" getestet
 		 */
 		
-		pe1.setBewertungen(4);								// settet die länge für den Bewertungsarray
-		pe1.neueBewertung(bewertung);						// Fügt eine neue Bewertung hinzu um die Methode durchschnittsbewertung austesten zu können
-		pe1.neueBewertung(bewertung2);						// Fügt eine neue Bewertung hinzu um die Methode durchschnittsbewertung austesten zu können
-		System.out.println(pe1.ermaessigterPreis());		// probiert die Methode ermaessigterPreis aus
+		pe1.setBewertungen(4);						// settet die lÃ¤nge fÃ¼r den Bewertungsarray
+		pe1.neueBewertung(bewertung);					// FÃ¼gt eine neue Bewertung hinzu um die Methode durchschnittsbewertung austesten zu kÃ¶nnen
+		pe1.neueBewertung(bewertung2);					// FÃ¼gt eine neue Bewertung hinzu um die Methode durchschnittsbewertung austesten zu kÃ¶nnen
+		System.out.println(pe1.ermaessigterPreis());			// probiert die Methode ermaessigterPreis aus
 		System.out.println(pe1.gesamtPreis(2));				// checkt nach ob die Methode gesamtPreis funktioniert
-		System.out.println(pe1.durchschnittsbewertung());	// gibt die Durchschnittsbewertung zurück
-		System.out.print(pe1.toString());					// gibt alle Daten in einem String zurück
-		System.out.println(pe1.equals(pe2));				// vergleicht ob die Methode equals aus der Klasse ErweitertesProdukt funktioniert (solte true zurückgeben)
-		System.out.println(pe1.equals(pe3));				// vergleicht ob die Methode equals aus der Klasse ErweitertesProdukt funktioniert (solte false zurückgeben)
-		System.out.println(pe1.hashCode());					// führt die Methode hashCode aus der Klasse "ErweitertesProdukt" aus
+		System.out.println(pe1.durchschnittsbewertung());		// gibt die Durchschnittsbewertung zurÃ¼ck
+		System.out.print(pe1.toString());				// gibt alle Daten in einem String zurÃ¼ck
+		System.out.println(pe1.equals(pe2));				// vergleicht ob die Methode equals aus der Klasse ErweitertesProdukt funktioniert (solte true zurÃ¼ckgeben)
+		System.out.println(pe1.equals(pe3));				// vergleicht ob die Methode equals aus der Klasse ErweitertesProdukt funktioniert (solte false zurÃ¼ckgeben)
+		System.out.println(pe1.hashCode());				// fÃ¼hrt die Methode hashCode aus der Klasse "ErweitertesProdukt" aus
 		/**
-		 * Hier werden alle Methoden für die Klasse: "ErweitertesProdukt2 getestet
+		 * Hier werden alle Methoden fÃ¼r die Klasse: "ErweitertesProdukt2 getestet
 		 */
-		System.out.println("Vergleich: " + bewertung2.equals(bewertung3));	// vergleicht ob die Methode equals in der Klasse Bewertung funktioniert (solte true zurückgeben)
-		System.out.println("Vergleich: " + bewertung.equals(bewertung2));	// vergleicht ob die Methode equals in der Klasse Bewertung funktioniert (solte true zurückgeben)
-		System.out.print(bewertung.hashCode());								// führt die Methode hashCode aus der Klasse "Bewertung" aus
+		System.out.println("Vergleich: " + bewertung2.equals(bewertung3));	// vergleicht ob die Methode equals in der Klasse Bewertung funktioniert (solte true zurÃ¼ckgeben)
+		System.out.println("Vergleich: " + bewertung.equals(bewertung2));	// vergleicht ob die Methode equals in der Klasse Bewertung funktioniert (solte true zurÃ¼ckgeben)
+		System.out.print(bewertung.hashCode());					// fÃ¼hrt die Methode hashCode aus der Klasse "Bewertung" aus
 		/**
-		 * Hier werden alle Methoden für die Klasse: "Bewertung" getestet
+		 * Hier werden alle Methoden fÃ¼r die Klasse: "Bewertung" getestet
 		 */
 	}
 }
