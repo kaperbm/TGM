f8d2ec2f7c49f8b109b0ca299cd060aa9e42c277
Kacper Bohaczyk <KacperBohaczyk@gmail.com>
2022-04-26 18:34:29 +0200

    Update Rechteck.java

--- 1_Jahrgang/G_2a_2_5_Rechteck-mit-Eingabe/Rechteck.java
+++ 1_Jahrgang/G_2a_2_5_Rechteck-mit-Eingabe/Rechteck.java
@@ -1,4 +1,4 @@
-package sew_1chit; // Das package einfach löschen falls in BlueJ programmiert werden muss
+package sew_1chit; // Das package einfach lÃ¶schen falls in BlueJ programmiert werden muss
 import javax.swing.JOptionPane;
 /**
  * Diese Klasse erstellt ein Rechteck welches vom User optimiert werden kann
@@ -8,18 +8,18 @@ import javax.swing.JOptionPane;
 public class Rechteck{
 	
     /**
-     * Die main Klasse nimmt einen Userinput auf. Dieser beinhalten welche breite das Rechteck haben soll, welche länge und aus welchem Zeichen es gebaut werden soll
+     * Die main Klasse nimmt einen Userinput auf. Dieser beinhalten welche breite das Rechteck haben soll, welche lÃ¤nge und aus welchem Zeichen es gebaut werden soll
      * @param args Argumente
      */
     public static void main(String[] args){
-        String breiteEing = JOptionPane.showInputDialog(null, "breite: ");     	// Nimmt einen Userinput auf für die Breite
-        String hoeheEing = JOptionPane.showInputDialog(null,"höhe: ");			// Nimmt einen Userinput auf für die Hoehe
-        String zeichen = JOptionPane.showInputDialog(null,"zeichen: ");			// Nimmt einen Userinput auf für das Zeichen
+        String breiteEing = JOptionPane.showInputDialog(null, "breite: ");     		// Nimmt einen Userinput auf fÃ¼r die Breite
+        String hoeheEing = JOptionPane.showInputDialog(null,"hÃ¶he: ");			// Nimmt einen Userinput auf fÃ¼r die Hoehe
+        String zeichen = JOptionPane.showInputDialog(null,"zeichen: ");			// Nimmt einen Userinput auf fÃ¼r das Zeichen
         for(int hoehe = Integer.parseInt(hoeheEing); hoehe>0; hoehe--){			
             for(int breite = Integer.parseInt(breiteEing); breite>0; breite--){	
-                System.out.print(zeichen);										// gibt ein Zeichen aus
+                System.out.print(zeichen);						// gibt ein Zeichen aus
             }
-            System.out.println();												// wechselt die Zeile (geht auch mit \n)
+            System.out.println();							// wechselt die Zeile (geht auch mit \n)
         }
     }
 }
