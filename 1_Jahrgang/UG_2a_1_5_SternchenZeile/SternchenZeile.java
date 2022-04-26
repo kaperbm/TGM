264186421b47f1e55fd19b888cec2124b96a986f
Kacper Bohaczyk <KacperBohaczyk@gmail.com>
2022-04-26 18:34:55 +0200

    Update SternchenZeile.java

--- 1_Jahrgang/UG_2a_1_5_SternchenZeile/SternchenZeile.java
+++ 1_Jahrgang/UG_2a_1_5_SternchenZeile/SternchenZeile.java
@@ -1,4 +1,4 @@
-package sew_1chit;		// Das Package einfach löschen falls die Aufgabe ohne einen Package sein soll
+package sew_1chit;		// Das Package einfach lÃ¶schen falls die Aufgabe ohne einen Package sein soll
 /**
  * Diese Klasse soll eine Zeile aus 9 Sternchen ausgeben
  * @author Kacper Bohaczyk
@@ -10,9 +10,9 @@ public class SternchenZeile {
 	 * @param args
 	 */
 	 public static void main(String[] args) {
-	     int laenge;							// Die Variable wird deklariert
+	     int laenge;				// Die Variable wird deklariert
 	     for(laenge=0; laenge<9; laenge++) {	// Der Vorgang wird mitels einer for Schleife 9x wiederholt
-	      System.out.print('*');				// Ein Sternchen wird ausgegeben
+	      System.out.print('*');			// Ein Sternchen wird ausgegeben
 	    }
 	}
 }
