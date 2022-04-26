3a4bbb7e4b0f4c09b608f40f6b93a3ce65781fd8
Kacper Bohaczyk <KacperBohaczyk@gmail.com>
2022-04-26 18:33:21 +0200

    Update SternchenRechteck.java

--- 1_Jahrgang/G_2a_1_7_SternchenRechteck/SternchenRechteck.java
+++ 1_Jahrgang/G_2a_1_7_SternchenRechteck/SternchenRechteck.java
@@ -1,4 +1,4 @@
-package sew_1chit; // Das package einfach löschen falls in BlueJ programmiert werden muss
+package sew_1chit; // Das package einfach lÃ¶schen falls in BlueJ programmiert werden muss
 /**
  * 
  * @author Kacper Bohaczyk
@@ -10,13 +10,13 @@ public class SternchenRechteck {
 	 * @param args Argumente
 	 */
 	 public static void main(String[] args) {
-	     int laenge;							// Die Variable wird deklariert
-	     int breite;							// Die Variable wird deklariert
+	     int laenge;				// Die Variable wird deklariert
+	     int breite;				// Die Variable wird deklariert
 	     for(breite=0; breite<5; breite++) {	// Der Vorgang wird solange wiederhollt bis 5 erreicht wird
 	     for(laenge=0; laenge<9; laenge++) {	// Der Vorgang wird solange wiederhollt bis 9 erreicht wird
-	      System.out.print('*');				// Ein Sternchen wird ausgegeben
+	      System.out.print('*');			// Ein Sternchen wird ausgegeben
 	    }
-	    System.out.println();					// Die Zeile wird gewechselt (geht auch mit einem \n)
+	    System.out.println();			// Die Zeile wird gewechselt (geht auch mit einem \n)
 	}
 	}
 }
